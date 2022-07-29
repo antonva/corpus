03f6187a136e410d355468e161590920de205df0
Anton Vilhelm Ásgeirsson <anton.v.asgeirsson@gmail.com>
2022-07-29 18:00:14 +0000

    Add calls without implementing them, flesh out errors and config

--- pallets/quadravote/src/lib.rs
+++ pallets/quadravote/src/lib.rs
@@ -8,6 +8,8 @@
 /// Then a voting period where identified account holders can reserve an amount
 /// of their tokens to vote on a particular subject.
 ///
+/// A proposer can withdraw their proposal if it is still the same proposal round
+///
 /// Definitions:
 ///
 /// An identified account holder is an account holder that has registered an identity
@@ -27,14 +29,21 @@ mod benchmarking;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::{dispatch::DispatchResultWithPostInfo, pallet_prelude::*};
+	use frame_support::{
+		dispatch::{DispatchResult, DispatchResultWithPostInfo},
+		pallet_prelude::*,
+		traits::ReservableCurrency,
+	};
 	use frame_system::pallet_prelude::*;
+	use sp_runtime::traits::AtLeast32BitUnsigned;
 
 	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
 		/// Because this pallet emits events, it depends on the runtime's definition of an event.
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+		type Token: ReservableCurrency<Self::AccountId>;
+		type ProposalRound: Parameter + AtLeast32BitUnsigned;
 	}
 
 	#[pallet::pallet]
@@ -57,11 +66,31 @@ pub mod pallet {
 		/// Event documentation should end with an array that provides descriptive names for event
 		/// parameters. [something, who]
 		SomethingStored(u32, T::AccountId),
+		// parameters: []
+		VoteRegistered(u32, T::AccountId),
 	}
 
 	// Errors inform users that something went wrong.
 	#[pallet::error]
-	pub enum Error<T> {}
+	pub enum Error<T> {
+		// We're not in the proposal period.
+		NotInProposalPeriod,
+
+		// We're not in the voting period.
+		NotInVotingPeriod,
+
+		// Account holder is not Identified.
+		NotIdentified,
+
+		// Voter does not have enough funds for this vote.
+		NotEnoughFunds,
+
+		// Proposal already exists when trying to add a new one.
+		ProposalAlreadyExists,
+
+		// Proposal doesn't exist, when trying to withdraw an existing one.
+		ProposalDoesNotExist,
+	}
 
 	#[pallet::hooks]
 	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
@@ -70,5 +99,24 @@ pub mod pallet {
 	// These functions materialize as "extrinsics", which are often compared to transactions.
 	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 	#[pallet::call]
-	impl<T: Config> Pallet<T> {}
+	impl<T: Config> Pallet<T> {
+		// Create a proposal that can be voted on
+		#[pallet::weight(1_000)]
+		pub fn create_proposal(origin: OriginFor<T>, proposal: Vec<u8>) -> DispatchResult {
+			Ok(())
+		}
+
+		// Withdraw a proposal if still within the same voting period.
+		// This should probably cost more to stop people from wasting others' time.
+		#[pallet::weight(10_000)]
+		pub fn withdraw_proposal(origin: OriginFor<T>, proposal: Vec<u8>) -> DispatchResult {
+			Ok(())
+		}
+
+		// Cast a vote on an existing proposal.
+		#[pallet::weight(1_000)]
+		pub fn cast_vote(origin: OriginFor<T>, proposal: Vec<u8>, amount: u32) -> DispatchResult {
+			Ok(())
+		}
+	}
 }
