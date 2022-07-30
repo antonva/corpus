25168552626118a61c781953c37c0b97ee288f6d
Anton Vilhelm Ásgeirsson <anton.v.asgeirsson@gmail.com>
2022-07-30 19:04:23 +0000

    Update generate chain specs script

--- scripts/generate-chain-spec.sh
+++ scripts/generate-chain-spec.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env sh
 
+PARA_ID=2000
+
 set -e
 
 # Generate the chain spec from the built node
 ./target/release/parachain-template-node build-spec > ./node/service/chain-specs/corpus.json
 
-# Generate a raw, distributable, chainspec with no bootnodes 
+sed -i "s/\"para_id\": 1000/\"para_id\": $PARA_ID/g" ./node/service/chain-specs/corpus.json
+sed -i "s/\"parachainId\": 1000/\"parachainId\": $PARA_ID /g" ./node/service/chain-specs/corpus.json
+sed -i "s/\"relay_chain\": \"rococo-local\"/\"relay_chain\": \"rococo_local_testnet\"/g" ./node/service/chain-specs/corpus.json
+
+# Generate a raw, distributable, chainspec with no bootnodes
 ./target/release/parachain-template-node build-spec \
 	--chain ./node/service/chain-specs/corpus.json \
 	--raw \
