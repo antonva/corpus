b52083f017400c6c78d748a9c9d1cbd9b2533eae
Anton Vilhelm Ásgeirsson <anton.v.asgeirsson@gmail.com>
2022-07-30 09:03:43 +0000

    Add the raw spec generation to the chain spec script

--- scripts/generate-chain-spec.sh
+++ scripts/generate-chain-spec.sh
@@ -5,8 +5,13 @@ set -e
 # Generate the chain spec from the built node
 ./target/release/parachain-template-node build-spec > ./node/service/chain-specs/corpus.json
 
-# Generate a raw chainspec with no bootnodes 
+# Generate a raw, distributable, chainspec with no bootnodes 
 ./target/release/parachain-template-node build-spec \
 	--chain ./node/service/chain-specs/corpus.json \
 	--raw \
-	--disable-default-bootnode > ./node/service/raw-chain-specs/corpus-raw.json
+	--disable-default-bootnode > ./node/service/raw-chain-specs/corpus-raw-distributable.json
+
+# Generate a raw chainspec
+./target/release/parachain-template-node build-spec \
+	--chain ./node/service/chain-specs/corpus.json \
+	--raw > ./node/service/raw-chain-specs/corpus-raw.json
