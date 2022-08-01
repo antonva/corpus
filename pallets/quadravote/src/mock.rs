786df3546fda1158a10c06733cedeef6ee83a8c8
Anton Vilhelm Ásgeirsson <anton.v.asgeirsson@gmail.com>
2022-08-01 20:47:20 +0000

    [quadravote] Introduce unit tests for proposals

--- pallets/quadravote/src/mock.rs
+++ pallets/quadravote/src/mock.rs
@@ -1,7 +1,12 @@
 use crate as pallet_quadravote;
-use frame_support::{dispatch::Vec, parameter_types, traits::Everything};
+use frame_support::{
+	dispatch::Vec,
+	parameter_types,
+	traits::{ConstU64, Everything, OnFinalize, OnInitialize},
+};
 use frame_system as system;
-use pallet_identity;
+use pallet_balances;
+use pallet_votingregistry;
 use sp_core::H256;
 use sp_runtime::{
 	testing::Header,
@@ -12,6 +17,7 @@ type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
 type Block = frame_system::mocking::MockBlock<Test>;
 
 pub(crate) type AccountId = u64;
+type Balance = u64;
 // Configure a mock runtime to test the pallet.
 frame_support::construct_runtime!(
 	pub enum Test where
@@ -19,51 +25,12 @@ frame_support::construct_runtime!(
 		NodeBlock = Block,
 		UncheckedExtrinsic = UncheckedExtrinsic,
 	{
-		System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
-		Quadravote: pallet_quadravote::{Pallet, Call, Storage, Event<T>},
-		// TODO: mock this and remove the dependency
-		VotingRegistry: pallet_votingregistry::{Pallet, Call, Storage, Event<T>}
+		System: frame_system,
+		Balances: pallet_balances,
+		Quadravote: pallet_quadravote,
+		VotingRegistry: pallet_votingregistry,
 	}
 );
-
-parameter_types! {
-	pub const PeriodLength: u32 = 5;
-	pub const MaxProposals: u8 = 10;
-	pub const MaxVotesPerAccount: u32 = 10;
-}
-
-impl pallet_quadravote::Config for Test {
-	type Currency = ();
-	type Event = Event;
-	type IdentityProvider = VotingRegistry;
-	type MaxProposals = MaxProposals;
-	type PeriodLength = PeriodLength;
-	type MaxVotesPerAccount = MaxVotesPerAccount;
-}
-
-parameter_types! {
-	pub const MaxRegistrars: u32 = 32;
-	pub const MaxSubAccounts: u32 = 0;
-	pub const SubAccountDeposit: u32 = 0;
-	pub const MaxAdditionalFields: u32 = 2;
-	pub const FieldDeposit: u32 = 0;
-	pub const BasicDeposit: u32 = 0;
-}
-
-impl pallet_identity::Config for Test {
-	type Event = Event;
-	type Currency = ();
-	type BasicDeposit = BasicDeposit;
-	type FieldDeposit = FieldDeposit;
-	type SubAccountDeposit = SubAccountDeposit;
-	type MaxSubAccounts = MaxSubAccounts;
-	type MaxAdditionalFields = MaxAdditionalFields;
-	type MaxRegistrars = MaxRegistrars;
-	type Slashed = (); // Don't do anything if account holders are slashed
-	type ForceOrigin = system::EnsureRoot<AccountId>;
-	type RegistrarOrigin = system::EnsureRoot<AccountId>;
-	type WeightInfo = pallet_identity::weights::SubstrateWeight<Test>;
-}
 
 parameter_types! {
 	pub const BlockHashCount: u64 = 250;
@@ -81,14 +48,14 @@ impl system::Config for Test {
 	type BlockNumber = u64;
 	type Hash = H256;
 	type Hashing = BlakeTwo256;
-	type AccountId = u64;
+	type AccountId = AccountId;
 	type Lookup = IdentityLookup<Self::AccountId>;
 	type Header = Header;
 	type Event = Event;
 	type BlockHashCount = BlockHashCount;
 	type Version = ();
 	type PalletInfo = PalletInfo;
-	type AccountData = ();
+	type AccountData = pallet_balances::AccountData<Balance>;
 	type OnNewAccount = ();
 	type OnKilledAccount = ();
 	type SystemWeightInfo = ();
@@ -97,7 +64,54 @@ impl system::Config for Test {
 	type MaxConsumers = frame_support::traits::ConstU32<16>;
 }
 
+// Quadravote parameter types
+parameter_types! {
+	pub const PeriodLength: u32 = 5;
+	pub const MaxProposals: u8 = 10;
+	pub const MaxVotesPerAccount: u32 = 10;
+}
+
+impl pallet_quadravote::Config for Test {
+	type Currency = ();
+	type Event = Event;
+	type IdentityProvider = VotingRegistry;
+	type MaxProposals = MaxProposals;
+	type PeriodLength = PeriodLength;
+	type MaxVotesPerAccount = MaxVotesPerAccount;
+}
+
+impl pallet_votingregistry::Config for Test {
+	type Event = Event;
+	type Currency = Balances;
+	type ReserveAmount = ConstU64<50>;
+}
+
+impl pallet_balances::Config for Test {
+	type MaxLocks = ();
+	type MaxReserves = ();
+	type ReserveIdentifier = [u8; 8];
+	type Balance = Balance;
+	type DustRemoval = ();
+	type Event = Event;
+	type ExistentialDeposit = ConstU64<1>;
+	type AccountStore = System;
+	type WeightInfo = ();
+}
+
 // Build genesis storage according to the mock runtime.
 pub fn new_test_ext() -> sp_io::TestExternalities {
 	system::GenesisConfig::default().build_storage::<Test>().unwrap().into()
+}
+
+// Helper function to fast forward to a specific block number.
+pub fn run_to_block(n: u64) {
+	while System::block_number() < n {
+		if System::block_number() > 1 {
+			Quadravote::on_finalize(System::block_number());
+			System::on_finalize(System::block_number());
+		}
+		System::set_block_number(System::block_number() + 1);
+		System::on_initialize(System::block_number());
+		Quadravote::on_initialize(System::block_number());
+	}
 }
--- pallets/quadravote/src/tests.rs
+++ pallets/quadravote/src/tests.rs
@@ -1,32 +1,81 @@
-use crate::{mock::*, Error};
-use frame_support::{assert_noop, assert_ok};
+use crate::{
+	mock::{run_to_block, *},
+	Error,
+};
+use frame_support::{assert_noop, assert_ok, error::BadOrigin};
 
 #[test]
-fn create_proposal_rejects_unsigned_origin() {
+fn call_create_proposal() {
 	new_test_ext().execute_with(|| {
-		// Dispatch a signed extrinsic.
+		let period_length = <Test as crate::Config>::PeriodLength::get();
+		let alice = 0u64;
+		// Unsigned fails
+		assert_noop!(Quadravote::create_proposal(Origin::none(), [0u8; 32]), BadOrigin);
+		// Signed fails while in the voting period
 		assert_noop!(
-			Quadravote::create_proposal(Origin::none(), 42),
-			Error::<Test>::ExtrinsicNotSigned
-		);
-	});
-}
-
-#[test]
-fn create_proposal_rejects_when_not_in_proposal_period() {
-	new_test_ext().execute_with(|| {
-		// Dispatch a signed extrinsic.
-		assert_noop!(
-			Quadravote::create_proposal(Origin::signed(1), 42),
+			Quadravote::create_proposal(Origin::signed(alice), [0u8; 32]),
 			Error::<Test>::NotInProposalPeriod
 		);
+		// Advance the chain by the period length to get to proposal period
+		run_to_block(period_length.into());
+		// Signed does not work if not identified by the voting registry
+		// Signed works in the proposal period
+		assert_ok!(Quadravote::create_proposal(Origin::signed(alice), [0u8; 32]));
+		// Submitting the same proposal fails
+		assert_noop!(
+			Quadravote::create_proposal(Origin::signed(alice), [0u8; 32]),
+			Error::<Test>::ProposalAlreadyExists
+		);
 	});
 }
 
-//#[test]
-//fn correct_error_for_none_value() {
-//	new_test_ext().execute_with(|| {
-//		// Ensure the expected error is thrown when no value is present.
-//		assert_noop!(Quadravote::cause_error(Origin::signed(1)), Error::<Test>::NoneValue);
-//	});
-//}
+#[test]
+fn call_create_proposal_maximum() {
+	new_test_ext().execute_with(|| {
+		let period_length = <Test as crate::Config>::PeriodLength::get();
+		let max_proposals = <Test as crate::Config>::MaxProposals::get();
+		let alice = 0u64;
+		// Advance the chain by the period length to get to proposal period
+		run_to_block(period_length.into());
+		for i in 0..max_proposals {
+			assert_ok!(Quadravote::create_proposal(Origin::signed(alice), [i as u8; 32]));
+		}
+		assert_noop!(
+			Quadravote::create_proposal(Origin::signed(alice), [10 as u8; 32]),
+			Error::<Test>::TooManyProposals
+		);
+	});
+}
+#[test]
+fn call_withdraw_proposal() {
+	new_test_ext().execute_with(|| {
+		let period_length = <Test as crate::Config>::PeriodLength::get();
+		let alice = 0u64;
+		let evelyn = 1u64;
+		// Unsigned fails
+		assert_noop!(Quadravote::withdraw_proposal(Origin::none(), [0u8; 32]), BadOrigin);
+		// Signed fails while in the voting period
+		assert_noop!(
+			Quadravote::withdraw_proposal(Origin::signed(alice), [0u8; 32]),
+			Error::<Test>::NotInProposalPeriod
+		);
+		// Advance the chain by the period length
+		run_to_block(period_length.into());
+		// Signed does not work if not identified by the voting registry
+
+		// Test above demonstrates that this works so unwrap is less icky
+		Quadravote::create_proposal(Origin::signed(alice), [0u8; 32]).unwrap();
+		// Withdrawing as another account id fails
+		assert_noop!(
+			Quadravote::withdraw_proposal(Origin::signed(evelyn), [0u8; 32]),
+			Error::<Test>::NotYourProposal
+		);
+		// Signed works in the proposal period
+		assert_ok!(Quadravote::withdraw_proposal(Origin::signed(alice), [0u8; 32]));
+		// Withdrawing a withdrawn proposal fails
+		assert_noop!(
+			Quadravote::withdraw_proposal(Origin::signed(alice), [0u8; 32]),
+			Error::<Test>::ProposalDoesNotExist
+		);
+	});
+}
